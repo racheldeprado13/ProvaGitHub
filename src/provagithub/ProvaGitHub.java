e53291c25a67f8e703e1397cbfed9cb7025e5952
alumne <alumne@alumne-PC>
2018-03-20 08:58:22 +0100

    creacio de projecte

---
+++ src/provagithub/ProvaGitHub.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagithub;
+
+/**
+ *
+ * @author alumne
+ */
+public class ProvaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Buenas GitHub!!");
+    }
+    
+}
