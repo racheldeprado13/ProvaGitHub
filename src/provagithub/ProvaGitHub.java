c26a20cea5f86097971da22e829b121e0ae3058f
alumne <alumne@alumne-PC>
2018-03-20 09:00:27 +0100

    creacio de projecte

--- src/provagithub/ProvaGitHub.java
+++ src/provagithub/ProvaGitHub.java
@@ -17,6 +17,8 @@ public class ProvaGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Buenas GitHub!!");
+        System.out.println("actualizació GitHub!");
+        
     }
     
 }
